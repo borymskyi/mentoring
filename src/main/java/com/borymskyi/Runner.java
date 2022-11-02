009027bf9cb1cc98f41bb42b93464392076329ea
borymskyi <dimokskyn@gmail.com>
2022-11-02 21:21:39 +0200

    initial repo

---
+++ src/main/java/com/borymskyi/Runner.java
@@ -0,0 +1,11 @@
+package com.borymskyi;
+
+/**
+ * @author Dmitrii Borymskyi
+ * @version 1.0
+ */
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
